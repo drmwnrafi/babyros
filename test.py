1cfac651f1cf2d11b9c09f1b495d5cfd0159a21e
drmwnrafi <drmwnnrafi@gmail.com>
2026-07-09 03:22:16 +0700

    fix: delete unnecessary file

--- test.py
+++
@@ -1,8 +0,0 @@
-from rich.progress import Progress
-import time
-
-with Progress() as p:
-    t = p.add_task("Processing...", total=100)
-    while not p.finished:
-        p.update(t, advance=1)
-        time.sleep(0.05)
